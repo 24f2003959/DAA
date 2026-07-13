0311643367d25064af60683239f947565fea45c7
Ahmed <ahdstnpat@gmail.com>
2026-07-13 12:05:22 +0530

    Corrected with template

--- 01-07-26 HomeWork/Stack/stack.h
+++ 01-07-26 HomeWork/Stack/stack.h
@@ -1,11 +1,37 @@
-#include <ostream>
+#include <iostream>
+
+template <typename T>
 class Stack {
-    public:
-        int size;
-        int top;
-        int *arr;
-        Stack(int);
-        void operator+(int);
-        void operator--(int);
-        friend std::ostream& operator<<(std::ostream& os, const Stack& s);
+public:
+    int size;
+    int top;
+    T *arr;
+
+    Stack(int size) {
+        this->size = size;
+        arr = new T[size];
+        top = -1;
+    }
+    void operator+(T x) {
+        if (top == size - 1) {
+            std::cout << "Stack overflow\n";
+            return;
+        }
+        arr[++top] = x;
+    }
+    void operator--(int) {
+        if (top == -1) {
+            std::cout << "Stack underflow\n";
+            return;
+        }
+        top--;
+    }
+    friend std::ostream& operator<<(std::ostream& os, const Stack<T>& s) {
+        os << "Stack elements: ";
+        for (int i = 0; i <= s.top; i++) {
+            os << s.arr[i] << " ";
+        }
+        os << "\n";
+        return os;
+    }
 };
--- 01-07-26 HomeWork/Stack/stack_demo.cpp
+++ 01-07-26 HomeWork/Stack/stack_demo.cpp
@@ -1,13 +1,27 @@
-#include<iostream>
+#include <iostream>
 #include "stack.h"
+
 int main() {
-    Stack st(10);
-    st+10;
-    st+20;
-    st+30;
-    std::cout<<st;
+    std::cout << "Integer Stack:\n";
+    Stack<int> st(10);
+    st + 10;
+    st + 20;
+    st + 30;
+    std::cout << st;
     st--;
     st--;
-    std::cout<<st;
+    std::cout << st;
+
+    std::cout << "\nDouble Stack\n";
+    Stack<double> double_st(5);
+    double_st + 3.14159;
+    double_st + 2.71828;
+    double_st + 1.61803;
+    std::cout << double_st;
+    double_st--;
+    double_st--;
+    std::cout << double_st;
+
+
     return 0;
 }
